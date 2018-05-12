66b12e332abd51d0122602371a25ee921ac835ad
Ramsey Jordan <ramsey.jordan.84@gmail.com>
2018-05-11 21:15:37 -0400

    Making progress

---
+++ main.js
@@ -0,0 +1,7 @@
+// var buzzWords = ['Google Apps Inegration', 'Email Integration', 'Self Service Portal', 'Mobile Access', 'Search Engine Optimization', 'Email Marketing'];
+
+// function randomWord(words) {
+//     return words[Math.floor(Math.random() * words.length)];
+// }
+
+// for(var x=0 x<20; x++)
