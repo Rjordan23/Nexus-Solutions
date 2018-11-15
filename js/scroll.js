428f2a8dfd1b647222520f072e3ae2e5fa95dedd
Ramsey Jordan <ramsey.jordan.84@gmail.com>
2018-11-14 23:51:53 -0500

    fixed scrolling and removed holding pafes

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,24 @@
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
+// $(document).on('click', 'a[href^="#"]', function (event) {
+//     event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1000);
-    });
+//     $('html, body').animate({
+//         scrollTop: $($.attr(this, 'href')).offset().top
+//     }, 1000);
+//     });
+
+$(document).ready(function(){
+
+    var scrollLink = $('.scroll');
+  
+    // Smooth Scrolling
+    scrollLink.click(function(e){
+      e.preventDefault();
+        $('body, html').animate({
+          scrollTop: $(this.hash).offset().top
+        }, 1000 )
+    })
+  
+  
+  })
     
 
